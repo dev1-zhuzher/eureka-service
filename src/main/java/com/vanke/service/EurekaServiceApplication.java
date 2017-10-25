7e1e1bc6f8ccfe265e6c37685d0f9a28e559558b
dev1-zhuzher <dev1-zhuzher@outlook.com>
2017-10-25 15:08:27 +0800

    添加项目

---
+++ src/main/java/com/vanke/service/EurekaServiceApplication.java
@@ -0,0 +1,20 @@
+package com.vanke.service;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Author: Leo
+ * @Blog: http://blog.csdn.net/lc0817
+ * @CreateTime: 2016/11/21 22:49
+ * @Description:
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServiceApplication.class, args);
+    }
+}
